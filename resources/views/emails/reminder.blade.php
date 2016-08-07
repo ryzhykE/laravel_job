12a3b85f29229c47ee2f8d7e64c9182584926f29
ryzhykE <stoyatboy@gmail.com>
2016-08-07 09:06:55 +0300

    Update reminder.blade.php

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,2 +1,2 @@
-Hello {{ $user->first_name }}, you are holding "{{ $book->title  }}" for more than 30 days!<br/>
+Hello {{ $user->first_name }}, you are holding "{{ $book->title  }}" for more than 30 days! <br/>
 Could you please return it to the library: {{ $url }} !
